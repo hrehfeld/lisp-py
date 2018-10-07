fb06f04f0e539830f92743c50df3caf62c1309aa
hrehfeld <hauke@haukerehfeld.de>
2018-10-07 14:35:38 +0200

    add: basic env & eval t

--- lisp/interpreter.py
+++ lisp/interpreter.py
@@ -1,17 +1,32 @@
-from .symbol import intern
+from .symbol import intern, Symbol
 
 
 def is_num(f):
     return isinstance(f, int) or isinstance(f, float)
 
 
-def eval(form):
+def symbolp(e):
+    return isinstance(e, Symbol)
+
+def eval(form, env):
     if is_num(form):
         return form
+    if symbolp(form):
+        if not form.s in env:
+            raise Exception('Symbol %s not found in env (Keys: %s)' % (form.s, ', '.join(env.keys())))
+        return env[form.s]
+        
 
+
+def base_env():
+    env = dict(
+        t=True
+    )
+    return env
 
 def interpret(forms):
+    env = base_env()
     r = None
     for form in forms:
-        r = eval(form)
+        r = eval(form, env)
     return r
