47a3a58ebfb36e00facde6a02a8b46aeab4a517b
hrehfeld <hauke@haukerehfeld.de>
2018-11-17 20:18:15 +0100

    chg: symbol uses structs

--- lisp/symbol.py
+++ lisp/symbol.py
@@ -1,22 +1,12 @@
+from .base import Struct
+
 symbols = {}
 
-SYMBOL = '__SYMBOL'
-
-def Symbol(s):
-    assert(s)
-    return (SYMBOL, s)
+Symbol, symbolp, (symbol_name, ), _symbol_setters = Struct('symbol', 'name')
 
 
 def intern(s):
     symbols.setdefault(s, Symbol(s))
     return symbols[s]
-
-
-def symbol_name(sym):
-    return sym[1]
-
-
-def symbolp(sym):
-    return isinstance(sym, tuple) and len(sym) == 2 and sym[0] is SYMBOL
 
 
