f426e0d6ce1c73d4abaf1cf46771d302ecb2fc2e
hrehfeld <hauke@haukerehfeld.de>
2018-12-03 16:10:28 +0100

    add: bind named-operator

--- lisp/interpreter.py
+++ lisp/interpreter.py
@@ -631,6 +631,8 @@ def base_env(args=[]):
     env_def(env, 'tuplep', tuplep)
     env_def(env, 'tuple?', tuplep)
 
+    env_def(env, 'named-operator?', named_operatorp)
+
     def numeric_op(op):
         def numeric_op(a, *args):
             r = a
