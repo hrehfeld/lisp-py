30ceaa767e193cbaf1f9f6bd3e1977550142756c
hrehfeld <hauke@haukerehfeld.de>
2018-12-03 15:02:52 +0100

    add: bind str

--- lisp/interpreter.py
+++ lisp/interpreter.py
@@ -527,6 +527,7 @@ def base_env(args=[]):
         return l
     env_def(env, 'extend', extend)
 
+    env_def(env, 'str', str)
     env_def(env, 'dict', dict)
     env_def(env, 'dict-setdefault', dict.setdefault)
     env_def(env, 'dict-get', dict.get)
