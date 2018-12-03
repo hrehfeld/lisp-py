ceab1a27bd1fc43392ca5f730f37c070119a3e32
hrehfeld <hauke@haukerehfeld.de>
2018-12-03 16:53:41 +0100

    cln: better repr printing

--- lisp/interpreter.py
+++ lisp/interpreter.py
@@ -526,7 +526,7 @@ def base_env(args=[]):
     env_def(env, 'extend', extend)
 
     env_def(env, 'str', str)
-    env_def(env, 'repr', repr)
+    env_def(env, 'repr', sexps_str)
 
     env_def(env, 'dict', dict)
     env_def(env, 'dict-setdefault', dict.setdefault)
