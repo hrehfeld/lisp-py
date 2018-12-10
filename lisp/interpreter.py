7e9cb1a539a0bb61ff12338749d94a850359f6bd
hrehfeld <hauke@haukerehfeld.de>
2018-12-10 14:13:52 +0100

    fix: allow nth for tuples

--- lisp/interpreter.py
+++ lisp/interpreter.py
@@ -642,7 +642,7 @@ def base_env(args=[]):
     env_def(env, 'contains?', has)
 
     def nth(i, l):
-        assert (is_list(l)),  sexps_str(l)
+        assert (is_list(l) or is_tuple(l)),  sexps_str(l)
         return l[i]
     env_def(env, 'nth', nth)
 
